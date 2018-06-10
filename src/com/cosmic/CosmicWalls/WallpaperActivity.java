a134fb1fee1c449bccde30f5bbbda97e54c684b7
Prashant Maheshwari <prashantqwerty4@gmail.com>
2018-06-10 05:04:22 -0700

    Update WallpaperActivity.java

--- src/com/cosmic/CosmicWalls/WallpaperActivity.java
+++ src/com/cosmic/CosmicWalls/WallpaperActivity.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package com.cosmic.CosmicWalls;
+package com.social.SocialWalls;
 
 import android.app.Activity;
 import android.app.AlertDialog;
