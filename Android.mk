87d9ae33b040f45fca1acf3d4837487ae8742bf4
Prashant Maheshwari <prashantqwerty4@gmail.com>
2018-06-10 05:00:34 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -29,6 +29,6 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.design
 
-LOCAL_PACKAGE_NAME := CosmicWalls
+LOCAL_PACKAGE_NAME := SocialWalls
 
 include $(BUILD_PACKAGE)
